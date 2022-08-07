2ea4aaffe763d7ce20cd3cbdf6293fcd6bf66f69
pankajs91 <110783578+pankajs91@users.noreply.github.com>
2022-08-07 23:29:43 +0530

    ds

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express= require('express')
 const cors= require('cors')
 const app = express();
-
+app.get('/',(req,res)=>{
+    res.send('ok');
+})
 app.listen(process.env.PORT||5000,()=>{
     console.log(`listening at port${process.env.PORT||5000}`)
 })
